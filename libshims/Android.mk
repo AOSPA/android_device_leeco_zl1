26a305dffed329b4f6730537dd9afb1314831f47
Marko Man <darkobas@gmail.com>
2017-06-02 22:11:57 +0200

    zl1: remove camera shim leftovers
    Change-Id: Ied96979fede5e5ee0c9020e320f90090f56b8c89
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -22,14 +22,6 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := camera_shim.cpp
-LOCAL_MODULE := libcamera_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_32_BIT_ONLY := true
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libwvm_shim.c
 LOCAL_SHARED_LIBRARIES := libstagefright_foundation
 LOCAL_MODULE := libwvm_shim
